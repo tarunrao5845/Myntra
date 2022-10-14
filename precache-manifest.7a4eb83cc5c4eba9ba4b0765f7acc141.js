311d39b753162c52d1d4dfc10e7da8d3905bc494
Tarun Rao <tarunprop123@gmail.com>
2022-10-14 11:45:44 +0530

    Updates

---
+++ precache-manifest.7a4eb83cc5c4eba9ba4b0765f7acc141.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49e5b67a644dcfd6ec2e",
+    "url": "/reactapp/static/css/main.e349f7f7.chunk.css"
+  },
+  {
+    "revision": "49e5b67a644dcfd6ec2e",
+    "url": "/reactapp/static/js/main.49e5b67a.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/reactapp/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "d58e3843a90c6656e3a5",
+    "url": "/reactapp/static/js/2.d58e3843.chunk.js"
+  },
+  {
+    "revision": "c60cec80ff2e14155e0d",
+    "url": "/reactapp/static/js/runtime~main.c60cec80.js"
+  },
+  {
+    "revision": "14034ab40aa20917c403c377eec68f3d",
+    "url": "/reactapp/static/media/section1img.14034ab4.png"
+  },
+  {
+    "revision": "f8f51da9cf33bb8b39ce63017b49406b",
+    "url": "/reactapp/static/media/img2.f8f51da9.png"
+  },
+  {
+    "revision": "f2fcd824e741206a3fe515b80a6e4abd",
+    "url": "/reactapp/index.html"
+  }
+];
